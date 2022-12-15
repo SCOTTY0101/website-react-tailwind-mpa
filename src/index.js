d93b2e2adf56fcd75ef0b5d6c68dbe525334e2a5
SCOTTY0101 <alan777555@hotmail.com>
2022-12-15 18:05:26 +1000

    testing

--- src/index.js
+++ src/index.js
@@ -9,14 +9,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <React.Fragment>
-      <>
-      <span>React Fragment testing: Span One. </span>
-      <span>React Fragment testing: Span Two. </span>
-      <div>React Fragment testing: Div One</div>
-      <div>React Fragment testing: Div Two</div>
-      </>
-      </React.Fragment>  
       <App />
       <Footer />
   </React.StrictMode>
