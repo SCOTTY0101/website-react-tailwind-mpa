b3cde8a46e7e449503ccd5e9e81784b304ed553f
SCOTTY0101 <alan777555@hotmail.com>
2022-12-31 07:28:01 +1000

    blog updates

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -26,7 +26,7 @@ export default function Blog() {
                         </div>
                        
                         <div className='bg-sky-100 rounded-lg border-2 border-slate-800'>
-                        <h3 className='font-bold underline p-2'>Why do I code?</h3>
+                        <h3 className='font-bold underline p-2'>Why do I write code?</h3>
                         <p className='p-2'>
                         It gives you the ability to think, how to break down problems and how to solve them.<br />
                         I enjoy challenges of working with pseudocode, algorithms and data structures.
@@ -39,7 +39,7 @@ export default function Blog() {
                         {"It's"} relatively easy to learn and whither {"it's"} a good or bad a thing, but JavaScript is everywhere.
                         Want to work Frontend with Web or Mobile {"App's"}?
                         Try React, Nextjs or React Native.
-                        Need files or data to the client-side or servers? JSON.  Back-end code with {"API's"}? NodeJS.
+                        Need files or data to transfer from the client-side or servers? JSON.  Back-end code with {"API's"}? NodeJS.
                         If you're in need of a Database, then try MongoDB.
                         </p>
                         </div>
