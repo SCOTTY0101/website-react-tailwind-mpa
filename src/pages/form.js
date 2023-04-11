50c7deac452977727006d3a81b307e854e1f51a8
SCOTTY0101 <alan777555@hotmail.com>
2023-04-11 21:31:34 +1000

    test html form

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,20 +1,33 @@
-import React, { useRef } from 'react';
+//import React, { useRef } from 'react';
+import React, { useState } from 'react'
 
 function UsernameForm() {
-  const inputRef = useRef();  //()
+  const [username, setUsername] = React.useState('');
+  const isLowercase = username === username.toLowerCase();
+  const error = isLowercase ? null : 'Username must be lowercase';
+ // const inputRef = useRef();  //(null)
 
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log(inputRef.current.value);
+    //const username = event.target.elements.usernameInput.value //not needed with useState hook
+   // console.log(inputRef.current.value);
+    console.log(`You entered ${username}`)
   };
+
+  function handleChange(event) {
+    setUsername(event.target.value)
+  }
 
   return (
     <>
     <form onSubmit={handleSubmit}>
-      <label>Name:</label>
-        <input type="text" ref={inputRef} />
-      </form>
-    <button type="submit" className="hover:opacity-80  bg-slate-200 border-2 border-slate-700 rounded-xl px-2">Submit</button>
+      <div>
+      <label htmlFor="usernameInput">Userame:</label>
+        <input id="usernameInput" type="text" onChange={handleChange} />
+        </div>
+      <div style={{color: 'red'}}>{error}</div>
+    <button disabled={Boolean(error)} type="submit" className="hover:opacity-80  bg-slate-200 border-2 border-slate-700 rounded-xl px-2">Submit</button>
+    </form>
     </>
    
   );
