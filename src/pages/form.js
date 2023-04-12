d8c65a2bcea0acc60f65261d8bfca7e424a51131
SCOTTY0101 <alan777555@hotmail.com>
2023-04-12 20:52:19 +1000

    useState testing

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,5 +1,5 @@
 //import React, { useRef } from 'react';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function UsernameForm() {
   const [username, setUsername] = React.useState('');
