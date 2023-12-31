a8a24e3c72a46a4f64eafd6cdb5ab51b06a6e272
SCOTTY0101 <alan777555@hotmail.com>
2023-12-31 12:42:20 +1000

    X test

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -55,12 +55,6 @@ const Navbar =() => {
                         <li className='pt-1 px-2 md:px-4'>
                             <a  className='flex justify-center items-center hover:opacity-70' href="https://twitter.com/AlanGeorge_BNE" target="_blank" rel="noopener noreferrer" title="twitter icons">
                                 <img src={TwitterX} alt="twitter logo"></img>
-                               {/* <svg width="16" height="16" viewBox="0 0 300 300" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34
-                                     0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 
-                                     116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/>
-                                </svg> */}
-    
                             </a>
                         </li>
 
